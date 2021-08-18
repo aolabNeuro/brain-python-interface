136d472bb566e7d6d76b33afbcfe256070b7a2f0
Leo Scholl <leo.scholl@gmail.com>
2021-08-18 16:44:07 -0700

    Shading issue possible fix

--- riglib/stereo_opengl/window.py
+++ riglib/stereo_opengl/window.py
@@ -391,8 +391,10 @@ class Window2D():
     '''
     def screen_init(self):
         super().screen_init()
-        #glBlendEquation(GL_MAX)
-        #glBlendFunc(GL_ONE, GL_ONE) # temporary solution to weird blending issue with spheres
+        # glBlendEquation(GL_MAX)
+        # glBlendFunc(GL_ONE, GL_ONE) # temporary solution to weird blending issue with spheres
+        glBlendEquation(GL_FUNC_ADD)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_CONSTANT_ALPHA) #Possible fix for shading issue
 
     def _get_renderer(self):
         return render.Renderer2D(self.screen_cm)
