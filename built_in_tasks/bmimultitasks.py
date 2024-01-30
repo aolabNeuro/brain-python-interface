84b170e0bd1228d5e9521184a27ff8932d26a7bc
katherineperks <leo.scholl@gmail.com>
2024-01-30 08:54:52 -0800

    comment

--- built_in_tasks/bmimultitasks.py
+++ built_in_tasks/bmimultitasks.py
@@ -334,22 +334,22 @@ class BMIControlMultiMixin(BMILoop, LinearlyDecreasingAssist):
         self.decoder.filt.state.mean = self.init_decoder_mean.copy()
         self.hdf.sendMsg("reset")
         
-    @control_decorator
-    def update_zscore(self):
-        # This is a temporary solution - LRS Jan 2024
-        # Only works if the decoder currently has mFR == 0 and sdFR == 1
-        # Assumes you're using a lindecoder with a reasonably large buffer (>2 minutes)
-        self.decoder.filt.fix_norm_attr() # Should be already!
-        self.decoder.filt._update_scale_attr()
-        print(f"updated zscore")
-        self.hdf.sendMsg(f"updated zscore")
-        mFR = self.decoder.filt.attr['offset'].copy()
-        sdFR = self.decoder.filt.attr['scale'].copy()
-        self.decoder.init_zscore(mFR, sdFR)
-        self.hdf.sendAttr("task", "session_mFR", mFR)
-        self.hdf.sendAttr("task", "session_sdFR", sdFR)
-        n_units = self.decoder.filt.n_units
-        self.decoder.filt.update_norm_attr(offset=np.zeros(n_units), scale=np.ones(n_units))
+    # @control_decorator
+    # def update_zscore(self):
+    #     # This is a temporary solution - LRS Jan 2024
+    #     # Only works if the decoder currently has mFR == 0 and sdFR == 1
+    #     # Assumes you're using a lindecoder with a reasonably large buffer (>2 minutes)
+    #     self.decoder.filt.fix_norm_attr() # Should be already!
+    #     self.decoder.filt._update_scale_attr()
+    #     print(f"updated zscore")
+    #     self.hdf.sendMsg(f"updated zscore")
+    #     mFR = self.decoder.filt.attr['offset'].copy()
+    #     sdFR = self.decoder.filt.attr['scale'].copy()
+    #     self.decoder.init_zscore(mFR, sdFR)
+    #     self.hdf.sendAttr("task", "session_mFR", mFR)
+    #     self.hdf.sendAttr("task", "session_sdFR", sdFR)
+    #     n_units = self.decoder.filt.n_units
+    #     self.decoder.filt.update_norm_attr(offset=np.zeros(n_units), scale=np.ones(n_units))
 
     @control_decorator
     def toggle_clda(self):
