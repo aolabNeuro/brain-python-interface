4275bbfc69dcd83885bca8ae3b1595571837dee5
Leo <leo.scholl@gmail.com>
2024-08-27 16:31:43 -0700

    working in test but address gets bound up

--- riglib/pupillabs/__init__.py
+++ riglib/pupillabs/__init__.py
@@ -1,4 +1,5 @@
 import time
+import traceback
 import numpy as np
 import zmq
 # from msgpack import loads
@@ -13,10 +14,10 @@ class System(DataSourceSystem):
     '''
     
     '''
-    dtype = np.dtype(('float', (8,)))
+    dtype = np.dtype((float, (6,)))
     update_freq = 200
     
-    def __init__(self, ip="127.0.0.1", port="50020"):
+    def __init__(self, ip="128.95.215.191", port="50020"):
         '''
         For eye tracking, need Pupil Capture running in the background (after calibration in Pupil Capture)
         '''
@@ -100,51 +101,31 @@ class System(DataSourceSystem):
         """
         read in a batch of eye data and retun x, y on surface & pupil diameters for each eye
         """
-        raw0, raw1 = (np.nan, np.nan)
+        raw = (np.nan, np.nan)
         confidence = np.nan
-        diameter0, diameter1 = np.nan
+        diameter0, diameter1 = (np.nan, np.nan)
         timestamp = np.nan
 
-        try:  # load messages from the surface subport, this updates 200Hz
-            while self.sub.poll(0) == zmq.POLLIN: # clear the buffer after
-                topic, payload = self.sub.recv_multipart(flags=zmq.NOBLOCK) #noblock for recv(), unless it will wait until messages come in
-                message = msgpack.loads(payload, raw=False)
-            
-                if topic.startswith(b"surfaces"): # get the surface datum when gaze in on the surface
-                    # self.mapper.update_homography(message["img_to_surf_trans"])
-                    #  "gaze_on_surfaces": (
-                    #     {
-                    #         "topic": "gaze.3d.1._on_surface",
-                    #         "norm_pos": (-0.6709809899330139, 0.41052111983299255),
-                    #         "confidence": 0.5594810076623645,
-                    #         "on_surf": False,
-                    #         "base_data": ("gaze.3d.1.", 714040.132285),
-                    #         "timestamp": 714040.132285,
-                    #     },
-                    if len(message["gaze_on_surfaces"]) == 2:
-                        raw0 = message["gaze_on_surfaces"][0]["norm_pos"]
-                        raw1 = message["gaze_on_surfaces"][0]["norm_pos"]
+        while self.sub.poll(1000./self.update_freq) == zmq.POLLIN: # clear the buffer after
+            topic, payload = self.sub.recv_multipart(flags=zmq.NOBLOCK) #noblock for recv(), unless it will wait until messages come in
+            message = msgpack.loads(payload, raw=False)
+        
+            if topic.startswith(b"surfaces"): # get the surface datum when gaze in on the surface
+                # self.mapper.update_homography(message["img_to_surf_trans"])
+                if not "gaze_on_surfaces" in message.keys():
+                    continue
+                for message in message["gaze_on_surfaces"]:
+                    if message["topic"].startswith("gaze.3d.01"):
+                        raw = message["norm_pos"]
                         timestamp = message["timestamp"]
+                        confidence = message["confidence"]
 
-                elif topic.startswith(b"pupil.0.2d"):
-                    diameter0 = float(message["diameter"]) # pupil 0 diamter, right eye, unit: pixel
-                elif topic.startswith(b"pupil.1.2d"):
-                    diameter1 = float(message["diameter"]) # pupil 1 diamter, left eye, unit: pixel
+            elif topic.startswith(b"pupil.0.2d"):
+                diameter0 = float(message["diameter"]) # pupil 0 diamter, right eye, unit: pixel
+            elif topic.startswith(b"pupil.1.2d"):
+                diameter1 = float(message["diameter"]) # pupil 1 diamter, left eye, unit: pixel
 
-                # elif topic.startswith(b"gaze"):
-                #     mapped_gaze = self.mapper.gaze_to_surface(message["norm_pos"])
-                #     if mapped_gaze is None: 
-                #         continue
-
-                #     #raw x, y coordinates from surface data, (0,0) is the bottom left corner of the surface and (1,1) is the top right corner
-                #     raw_x = float(mapped_gaze.norm_x)
-                #     raw_y = float(mapped_gaze.norm_y)
-                #     confidence = float(message["confidence"])  # quaulity of measurements, [0,1]            
-
-        except: # mannually set x and y to be nan when no pupil data is updated
-            pass
-
-        coords = [raw0[0], raw0[1], raw1[0], raw1[1], diameter0, diameter1, confidence, timestamp]
+        coords = [raw[0], raw[1], diameter0, diameter1, confidence, timestamp]
         coords = np.expand_dims(coords, axis=0)
         return coords
 
