d5d7c55794f21ca45ef6f7cccd7209a0ab623d35
Leo <leo.scholl@gmail.com>
2025-04-04 14:48:25 -0700

    add to list

--- built_in_tasks/target_capture_task.py
+++ built_in_tasks/target_capture_task.py
@@ -270,7 +270,7 @@ class ScreenTargetCapture(TargetCapture, Window):
 
     sequence_generators = [
         'out_2D', 'centerout_2D', 'centeroutback_2D', 'centerout_2D_select', 'rand_target_chain_2D', 
-        'rand_target_chain_3D', 'corners_2D', 'centerout_tabletop',
+        'rand_target_chain_3D', 'corners_2D', 'corners_3D', 'centerout_tabletop',
     ]
 
     hidden_traits = ['cursor_color', 'target_color', 'cursor_bounds', 'cursor_radius', 'plant_hide_rate', 'starting_pos']
