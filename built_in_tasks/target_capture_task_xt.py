cde19fa7a11cdf21bc9ed13e3aa99864908d0c57
mtringi <mlt37@uw.edu>
2026-02-12 12:59:46 -0800

    remove target off sync event

--- built_in_tasks/target_capture_task_xt.py
+++ built_in_tasks/target_capture_task_xt.py
@@ -705,7 +705,7 @@ class ScreenTargetCapture_ReadySet(ScreenTargetCapture):
         pass
     
     def _start_targ_transition(self):
-        super()._start_targ_transition()
+        #super()._start_targ_transition()
         if self.target_index == -1:   # Came from a penalty state
             pass
 
