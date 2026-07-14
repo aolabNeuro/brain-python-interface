b9a5cbfaca05a08c3db87aa8209153a3adf78326
sofie <sofieds@uw.edu>
2026-07-14 10:32:52 -0700

    moved the load_decoder and null flag into our task to make sure we don't impact anbody elsese tasks. Need to check with Leo about the bmimultitask flag to ensure there aren't any other weird interactio nI don't know about

--- built_in_tasks/passivetasks.py
+++ built_in_tasks/passivetasks.py
@@ -42,9 +42,7 @@ class EndPostureFeedbackController(BMILoop, traits.HasTraits):
         units = []
         self.decoder = Decoder(filt, units, self.ssm, binlen=1./self.decoder_update_rate)
         self.decoder.n_features = 1
-        print(self.decoder.filt)
-        #This flag is to indicate that it was an assist an not necessary to be saved.
-        self.decoder.is_null_decoder = True
+        
 
     def create_feature_extractor(self):
         self.extractor = DummyExtractor()
@@ -66,6 +64,16 @@ class TargetCaptureVisualFeedbackEyeConstrained(EndPostureFeedbackController, BM
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.most_recent_open_eye = 0
+
+    def load_decoder(self):
+        self.ssm = StateSpaceEndptVel2D()
+        A, B, W = self.ssm.get_ssm_matrices()
+        filt = MachineOnlyFilter(A, W)
+        units = []
+        self.decoder = Decoder(filt, units, self.ssm, binlen=1./self.decoder_update_rate)
+        self.decoder.n_features = 1
+        #This flag is to indicate that it was an assist an not necessary to be saved.
+        self.decoder.is_null_decoder = True
 
     status = dict(
         wait = dict(start_trial="target", start_pause="pause"),
